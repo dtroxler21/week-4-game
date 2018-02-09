bbb875ead601ed7cba4667e623f4e5825a3e6c8b
dtroxler21 <dtroxler21@gmail.com>
2018-02-09 10:53:59 -0500

    fixing problem with display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,43 +11,43 @@ $(document).ready(function() {
 
 		//Creating connection to html for later
 		html: {
-			winCounter: $("#winCounter"),
-			lossCounter: $("#lossCounter"),
-			targetScore: $("#targetScore"),
-			totalScore: $("#totalScore"),
+			winCounter: $("#js-wins"),
+			lossCounter: $("#js-losses"),
+			targetScore: $("#js-target-score"),
+			totalScore: $("#js-total-score"),
 		},
 
 		//Function to start game including assigning values to crystals, targetScore, etc.
 		startGame: function() {
-			this.totalScore = 0;
-			this.targetScore = Math.floor(Math.random() * 102) +19;
+			crystal.totalScore = 0;
+			crystal.targetScore = Math.floor(Math.random() * 102) +19;
 			for (var i = 0; i < 4; i++) {
-				this.crystalValues[i] = Math.floor(Math.random() * 12) + 1;
-				this.crystalArray[i].attr("crystal-values", this.crystalValues[i]);
+				crystal.crystalValues[i] = Math.floor(Math.random() * 12) + 1;
+				crystal.crystalArray[i].attr("crystal-values", crystal.crystalValues[i]);
 			};
-			this.html.winCounter.text(this.wins);
-			this.html.lossCounter.text(this.losses);
-			this.html.targetScore.text(this.targetScore);
-			this.html.totalScore.text(this.totalScore);
+			crystal.html.winCounter.text(crystal.wins);
+			crystal.html.lossCounter.text(crystal.losses);
+			crystal.html.targetScore.text(crystal.targetScore);
+			crystal.html.totalScore.text(crystal.totalScore);
 		},
 
 		//Function to change values based on user clicking on crystals
 		crystalClick: function() {
-			this.html.totalScore.text(this.totalScore);
+			crystal.html.totalScore.text(crystal.totalScore);
 		},
 
 		//Function for finishing a game
 		finishGame: function() {
-			if (this.totalScore === this.targetScore) {
+			if (crystal.totalScore === crystal.targetScore) {
 				alert("You win!");
-				this.wins++;
-				this.html.winCounter.text(this.wins);
-				this.startGame();
-			} else if (this.totalScore > this.targetScore) {
+				crystal.wins++;
+				crystal.html.winCounter.text(crystal.wins);
+				crystal.startGame();
+			} else if (crystal.totalScore > crystal.targetScore) {
 				alert("You lose!");
-				this.losses++;
-				this.html.lossCounter.text(this.losses);
-				this.startGame();
+				crystal.losses++;
+				crystal.html.lossCounter.text(crystal.losses);
+				crystal.startGame();
 			};
 		}
 
@@ -58,7 +58,7 @@ $(document).ready(function() {
 
 	//Creating on click events for the crystals
 	$(".crystals").on("click", function() {
-		var crystalValue = $(this).attr("crystal-values");
+		var crystalValue = $(crystal).attr("crystal-values");
 		crystalValue = parseInt(crystalValue);
 		crystal.totalScore += crystalValue;
 		crystal.crystalClick();
